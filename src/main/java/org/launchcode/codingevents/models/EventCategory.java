c79f6f33abf78e85d0c23f9cbe08665423a46688
Pearce Odegard <69996490+podegard89@users.noreply.github.com>
2021-07-27 17:08:01 -0500

    In class solution done

---
+++ src/main/java/org/launchcode/codingevents/models/EventCategory.java
@@ -0,0 +1,36 @@
+package org.launchcode.codingevents.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class EventCategory {
+
+    @Id
+    @GeneratedValue
+    private int id;
+    private String name;
+
+    public EventCategory(String name) {
+        this.name = name;
+    }
+
+    public EventCategory() {}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
